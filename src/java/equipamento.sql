d13957735c772e0c0347fe2803f33898d340bed2
BrunahAlves <brunahalves94@gmail.com>
2017-03-31 21:52:44 -0300

    Edita equipamento.sql

--- src/java/equipamento.sql
+++ src/java/equipamento.sql
@@ -1,9 +1,10 @@
-CREATE TABLE ​equipamento
+CREATE TABLE equipamento
 (
-id ​ ​INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-serie ​ ​VARCHAR(100) NOT NULL,
-local ​ ​VARCHAR(250) NOT NULL,
-descricao ​ ​VARCHAR(32672) NOT NULL,
-estado ​ ​INT NOT NULL DEFAULT 0
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    serie VARCHAR(100) NOT NULL,
+    local VARCHAR(250) NOT NULL,
+    descricao VARCHAR(32672) NOT NULL,
+    estado INT NOT NULL DEFAULT 0
 );
 
+--INSERT INTO equipamento(serie, local, descricao, estado) VALUES('ABC4874', 'predio 1', 'computador', 0)
