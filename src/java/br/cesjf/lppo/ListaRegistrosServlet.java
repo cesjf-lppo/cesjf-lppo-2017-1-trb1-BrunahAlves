4f4aa303f7afb162c8d0a02744af3b3953ded721
brunahalves <brunahalves94@gmail.com>
2017-04-07 20:36:42 -0300

    Edita Filtro ListaRegistrosServlet

--- src/java/br/cesjf/lppo/ListaRegistrosServlet.java
+++ src/java/br/cesjf/lppo/ListaRegistrosServlet.java
@@ -31,7 +31,7 @@ public class ListaRegistrosServlet extends HttpServlet {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             Connection conexao = DriverManager.getConnection("jdbc:derby://localhost:1527/lppo-2017-1", "usuario", "senha");
             Statement operacao = conexao.createStatement();
-            ResultSet resultado = operacao.executeQuery("SELECT * FROM equipamento " + filtro);
+            ResultSet resultado = operacao.executeQuery("SELECT * FROM equipamento" + filtro + "ORDER BY local");
 
             while (resultado.next()) {
                 Equipamento equipamento = new Equipamento();
